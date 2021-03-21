b5843dc808ec98a8ccc0e6cbf247a0982b932030
Thiago Kanagushiku <thiagokanagushiku@users.noreply.github.com>
2021-03-21 15:58:01 -0300

    Type of data

---
+++ Origamid/Básico/2. Tipos-de-dados/script.js
@@ -0,0 +1,37 @@
+//Tipos primitivos de dados 
+
+//String 
+var nome = `João`
+var sobrenome = `Silva`
+var nomeCompleto = `${nome} ${sobrenome}`
+console.log(`O nome completo do cliente é: ${nomeCompleto}`)
+
+//Number
+var ano = `2020`;
+var idade = 24;
+
+//Verificador de tipo
+var frase = `It's time`
+var verificarTipoNome = typeof nome
+console.log(verificarTipoNome)
+
+//Ou
+var frase2 = 'Não sei'
+console.log(typeof frase2)
+
+//Boolean
+var possuiFaculdade = true
+
+//Undefined
+var time
+
+//Null
+var comida = null
+
+//Simbol
+var simbolo = Symbol()
+
+//Object
+var newObject = {
+
+}
